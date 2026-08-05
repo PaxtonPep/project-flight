072b11c8234092abc949230425b80cec1c51d664
PaxtonPep <peperzakpaxton06@gmail.com>
2026-08-05 10:45:36 -0700

    Update airport data and add new airports
    Updated departure and arrival charts for existing airports and added new airports with their respective details.

--- airports.js
+++ airports.js
@@ -2,6 +2,7 @@
 // This is the ONLY file you edit.
 
 const airports = [
+
     {
         name: "Larnaca",
         icao: "LCLK",
@@ -9,13 +10,13 @@ const airports = [
         departures: [
             "insert image link",   // departure chart 1
             "insert image link",   // departure chart 2
-            "none"                 // departure chart 3
+            "insert image link"    // departure chart 3
         ],
 
         arrivals: [
             "insert image link",   // arrival chart 1
-            "none",                // arrival chart 2
-            "none"                 // arrival chart 3
+            "insert image link",   // arrival chart 2
+            "insert image link"    // arrival chart 3
         ],
 
         taxi_chart: "insert image link",
@@ -35,5 +36,136 @@ const airports = [
         taxi_gate_same: "y"
     },
 
-    // Add more airports here using the same format
+    {
+        name: "RAF Akrotiri",
+        icao: "LCRA",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "y"
+    },
+
+    {
+        name: "Arroyo Barril",
+        icao: "MDAB",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "y"
+    },
+
+    {
+        name: "Cabo Rojo",
+        icao: "MDCR",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "y"
+    },
+
+    {
+        name: "Cibao",
+        icao: "MDST",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "n"
+    },
+
+    {
+        name: "Punta Cana",
+        icao: "MDPC",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "n"
+    },
+
+    {
+        name: "Kittilä",
+        icao: "EFKT",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "n"
+    },
+
+    {
+        name: "Antoine-Simon",
+        icao: "MTCA",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "y"
+    },
+
+    {
+        name: "Gran Canaria",
+        icao: "GCLP",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "n"
+    },
+
+    {
+        name: "Menorca",
+        icao: "LEMH",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "n"
+    },
+
+    {
+        name: "Gatwick",
+        icao: "EGKK",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "n"
+    },
+
+    {
+        name: "Southampton",
+        icao: "EGHI",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "y"
+    }
+
 ];
