6d50f4b602badbfb8fab2dba7e2cbc39d98e2d7e
PaxtonPep <peperzakpaxton06@gmail.com>
2026-08-05 10:42:08 -0700

    Update airports.js

--- airports.js
+++ airports.js
@@ -1,1 +1,39 @@
+// airports.js
+// This is the ONLY file you edit.
 
+const airports = [
+    {
+        name: "Larnaca",
+        icao: "LCLK",
+
+        departures: [
+            "insert image link",   // departure chart 1
+            "insert image link",   // departure chart 2
+            "none"                 // departure chart 3
+        ],
+
+        arrivals: [
+            "insert image link",   // arrival chart 1
+            "none",                // arrival chart 2
+            "none"                 // arrival chart 3
+        ],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "n"
+    },
+
+    {
+        name: "Paphos",
+        icao: "LCPH",
+
+        departures: ["none", "none", "none"],
+        arrivals: ["none", "none", "none"],
+
+        taxi_chart: "insert image link",
+        gate_chart: "insert image link",
+        taxi_gate_same: "y"
+    },
+
+    // Add more airports here using the same format
+];
